540a2b058e22570a56cdd712c709d86b76e5b7f5
Rolf van de Krol <info@rolfvandekrol.nl>
2015-09-27 17:39:12 +0200

    Add closed property on bug

--- lib/zoho/data/bug.rb
+++ lib/zoho/data/bug.rb
@@ -3,8 +3,12 @@ module Zoho
   module Data
     class Bug < Base
       parents :portal, :project
-      property :title
+      property :title, :closed
       child :log
+
+      def closed?
+        closed
+      end
     end
   end
 end

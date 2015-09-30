1d54f13947e802b539bbd5432ce51cd4759de6d6
Rolf van de Krol <info@rolfvandekrol.nl>
2015-09-30 21:46:13 +0200

    Add group name to project

--- lib/zoho/data/project.rb
+++ lib/zoho/data/project.rb
@@ -3,7 +3,7 @@ module Zoho
   module Data
     class Project < Base
       parents :portal
-      property :name, :link, :group_id
+      property :name, :link, :group_id, :group_name
       child :activity, :bug
     end
   end
